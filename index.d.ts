2945b03de1f3e24bd6e3151f402cf8b8b32f4b1e
Martin Heidegger <martin.heidegger@gmail.com>
2019-03-08 11:29:05 +0900

    fix: typescript definitions

--- index.d.ts
+++ index.d.ts
@@ -4,18 +4,20 @@ type straightProcess <InType> = (data: InType, callback: straightCb) => void
 type process <InType, OutType> = (data: InType, callback: callback<OutType>) => void
 
 export interface MapEach {
-  <InType> (items: Iterable<InType>, process: straightProcess<InType>, next: straightCb): void
-  <InType> (items: Iterable<InType>, process: straightProcess<InType>, concurrency: number, next: straightCb): void
-  <InType> (items: Iterable<InType>, process: straightProcess<InType>, next: straightCb, concurrency: number): void
-
+  // Without a callback it will return a promise
   <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>): Promise<Iterable<OutType>>
-  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, next: null | undefined): Promise<Iterable<OutType>>
-  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, next: callback<Iterable<OutType>>): void
+  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, cb: null | undefined): Promise<Iterable<OutType>>
+
+  // There is an optional "currency" that can be before or after the callback
   <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, concurrency: number): Promise<Iterable<OutType>>
-  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, concurrency: number, next: callback<Iterable<OutType>>): void
-  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, concurrency: number, next: null | undefined): Promise<Iterable<OutType>>
-  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, next: callback<Iterable<OutType>>, concurrency: number): void
-  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, next: null | undefined, concurrency: number): Promise<Iterable<OutType>>
+  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, concurrency: number, cb: null | undefined): Promise<Iterable<OutType>>
+  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, cb: null | undefined, concurrency: number): Promise<Iterable<OutType>>
+
+  // If a callback is provided, no promise is returned
+  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, cb: callback<Iterable<OutType>>): void
+  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, concurrency: number, cb: callback<Iterable<OutType>>): void
+  <InType, OutType> (items: Iterable<InType>, process: process<InType, OutType>, cb: callback<Iterable<OutType>>, concurrency: number): void
+
 }
 
 export declare const mapEach: MapEach
